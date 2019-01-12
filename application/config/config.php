8c021bf79925be8318f0a78dc8b8fbb2480dde8c
Adnan <eng.andnsafadi@gmail.com>
2019-01-12 14:50:51 +0200

    change url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Schools/';
+$config['base_url'] = 'http://142.93.128.173/Schools/';
 
 /*
 |--------------------------------------------------------------------------
