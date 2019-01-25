a2a951b1528cec0c83d1a239be3dbcd9fb920589
adnan <adoon194@gmail.com>
2019-01-25 15:10:44 +0200

    edit api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -4,48 +4,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Api extends CI_Controller {
 
 
-	function __construct() {
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+public function __construct()
+	{
 		parent::__construct();
-		$this->load->model('Schools_model');
+       $this->load->model('Schools_Core_model');
+		
 	}
 
-
-	// public function orders() {
-	// 	$this->load->model('Ze_Core_model');
-
-	// 	$data = array('username' => $this->input->post('username'),
-	// 		'address' => $this->input->post('address'),
-	// 		'menu_item' => $this->input->post('menu_item'),
-	// 		'notes' => $this->input->post('notes')
-	// 	);
-	// 	$out = array('res' => $this->Ze_Core_model->set_orders($data));
-	// 	echo json_encode($out);
-	// }
-
-	function appAuth(){
-		$this->load->model('Schools_model');
-		$email = $this->input->post('email');
-		$password = $this->input->post('password');
-
-		$this->load->library('form_validation');
-		$this->form_validation->set_rules('name', 'lang:name', 'required');
-		$this->form_validation->set_rules('password', 'lang:password', 'required|min_length[6]|max_length[30]');
-		
-		if ($this->form_validation->run() == TRUE)
-		{ 
-			
-			$output = array('status' => false, 'data' => "", "message" => "500");
-			
-		} else {
-		
-		$data = $this->Schools_model->app_auth_modle($email,$password) == null ? false: true;
-		$output = array('status' => $data, 'data' => $this->Schools_model->app_auth_modle($email,$password), "message" => "");
-
+	
+	public function index()
+	{
+		$this->load->view('welcome_message');
 	}
-	echo json_encode($output);
-}
 
-public function student_point_list() {
+ public function menu_list() {
  	
  	//echo "string";
  	$student_id = $this->input->post("student_id"); 
