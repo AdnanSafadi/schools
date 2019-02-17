968c5690a801524b9fdcdee64fc7a71bc763571b
Adnan <eng.andnsafadi@gmail.com>
2019-02-17 18:53:31 +0200

    set student point

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -20,10 +20,10 @@ class Api extends CI_Controller {
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 
-public function __construct()
+	public function __construct()
 	{
 		parent::__construct();
-       $this->load->model('Schools_Core_model');
+		$this->load->model('Schools_Core_model');
 		
 	}
 
@@ -33,44 +33,61 @@ public function __construct()
 		$this->load->view('welcome_message');
 	}
 
- public function student_point_list() {
- 	
+	public function student_point_list() {
+
  	//echo "string";
- 	$student_id = $this->input->post("student_id"); 
- 
-    $this->load->model('Schools_Core_model');
-    $output = $this->Schools_Core_model->get_student_point($student_id);
-    $out = array('data' => $output ,'status' => true, 'message' => '');
-    echo json_encode($out);
-}
+		$student_id = $this->input->post("student_id"); 
 
-public function check_student_auth() {
-	$student_id = $this->input->post("student_id"); 
-	$password = $this->input->post("password");
-    $output = $this->Schools_Core_model->check_student($student_id,$password);
-    if ($output == false) {
-    	 $out = array('data' => '' ,'status' => false, 'message' => '505');
-    }else {
-    	 $out = array('data' => $output ,'status' => true, 'message' => '');
-    }
-   
-    echo json_encode($out); 
-}
+		$this->load->model('Schools_Core_model');
+		$output = $this->Schools_Core_model->get_student_point($student_id);
+		$out = array('data' => $output ,'status' => true, 'message' => '');
+		echo json_encode($out);
+	}
 
-public function get_all_material(){
-	$student_id = $this->input->post("student_id"); 
-	$output = $this->Schools_Core_model->get_all_material($student_id);
-    $out = array('data' => $output ,'status' => true, 'message' => '');
-    echo json_encode($out);
-}
+	public function check_student_auth() {
+		$student_id = $this->input->post("student_id"); 
+		$password = $this->input->post("password");
+		$output = $this->Schools_Core_model->check_student($student_id,$password);
+		if ($output == false) {
+			$out = array('data' => '' ,'status' => false, 'message' => '505');
+		}else {
+			$out = array('data' => $output ,'status' => true, 'message' => '');
+		}
+
+		echo json_encode($out); 
+	}
+
+	public function get_all_material(){
+		$student_id = $this->input->post("student_id"); 
+		$output = $this->Schools_Core_model->get_all_material($student_id);
+		$out = array('data' => $output ,'status' => true, 'message' => '');
+		echo json_encode($out);
+	}
 
 
-public function get_student_data(){
-	$student_id = $this->input->post("student_id"); 
-	$output = $this->Schools_Core_model->get_student_data_model($student_id);
-    $out = array('data' => $output ,'status' => true, 'message' => '');
-    echo json_encode($out);
-}
+	public function get_student_data(){
+		$student_id = $this->input->post("student_id"); 
+		$output = $this->Schools_Core_model->get_student_data_model($student_id);
+		$out = array('data' => $output ,'status' => true, 'message' => '');
+		echo json_encode($out);
+	}
+
+	public function set_student_point(){
+		$student_id = $this->input->post("student_id"); 
+		$material_num = $this->input->post("material_num"); 
+		$point = $this->input->post("point"); 
+		$reason = $this->input->post("reason");
+
+		$data = $this->Schools_Core_model->set_student_point_model($student_id, $material_num, $point,$reason);
+
+		if ($data == false) {
+			$out = array('data' => '' ,'status' => false, 'message' => '');
+		}else {
+			$out = array('data' => $data ,'status' => true, 'message' => '');
+		}
+
+		echo json_encode($out);
+	}
 
 	
 }
--- application/models/Schools_Core_model.php
+++ application/models/Schools_Core_model.php
@@ -9,78 +9,108 @@
 class Schools_Core_model extends CI_Model {
 
 
-public function get_student_point($student_id){
+  public function get_student_point($student_id){
         // $query = $this->db->query('SELECT st.id, st.material_id as materials , st.point as studentPoint ,m.point as materialPoint FROM `student_point` as st INNER JOIN materials AS m ON m.id = st.student_id WHERE st.id = '.$student_id);
 
         //  return $query->result();
 
-         $this->db->select('student_point.id, materials.material_number as materials , materials.point as studentPoint ,materials.point as materialPoint');    
-         $this->db->from('student_point');
-         $this->db->join('materials', 'student_point.material_id = materials.id');
-         $this->db->join('student', 'student.id = student_point.student_id');
-        $this->db->where('student.user_key =', $student_id);
-        $query = $this->db->get();
-        return $query->result();
+   $this->db->select('student_point.id, materials.material_number as materials , materials.point as studentPoint ,materials.point as materialPoint');    
+   $this->db->from('student_point');
+   $this->db->join('materials', 'student_point.material_id = materials.id');
+   $this->db->join('student', 'student.id = student_point.student_id');
+   $this->db->where('student.user_key =', $student_id);
+   $query = $this->db->get();
+   return $query->result();
 
-    }
-	
-	public function check_student($student_id,$password){
-    	 $this->db->select('user_key,first_name,last_name,father_name,email,address,image');
-         $this->db->from('student');
-         $this->db->where('user_key =', $student_id);
-         $this->db->where('password =', $password);
-         $query = $this->db->get();
+ }
 
-         if ($query->num_rows() > 0) {
-         	return $query->row();
-         } else {
-         	return false;
-         }
-    }
+ public function check_student($student_id,$password){
+  $this->db->select('user_key,first_name,last_name,father_name,email,address,image');
+  $this->db->from('student');
+  $this->db->where('user_key =', $student_id);
+  $this->db->where('password =', $password);
+  $query = $this->db->get();
 
-    public function get_all_material($student_id) {
-        $this->db->select('*');
-        $this->db->from('materials');
-        $query = $this->db->get();
-        $data = $query->result();
+  if ($query->num_rows() > 0) {
+    return $query->row();
+  } else {
+    return false;
+  }
+}
+
+public function get_all_material($student_id) {
+  $this->db->select('*');
+  $this->db->from('materials');
+  $query = $this->db->get();
+  $data = $query->result();
        // die( $data['id'] );
 
-        foreach ($data as $variable) {
-           
-           $check_student =  $this->checkIfIsExistInStudentPoint($student_id,$variable->id);
+  foreach ($data as $variable) {
+
+   $check_student =  $this->checkIfIsExistInStudentPoint($student_id,$variable->id);
            //die($check_student);
-           if ($check_student) {
-            $variable->updated_at = true;
-           }
-        }
-        return $data;
-    }
+   if ($check_student) {
+    $variable->updated_at = true;
+  }
+}
+return $data;
+}
 
-    public function get_student_data_model($student_id) {
-       $this->db->select('*');
-       $this->db->from('student');
-       $this->db->where('user_key =',$student_id);
-       $query = $this->db->get();
-       return $data = $query->result();
-   }
+public function get_student_data_model($student_id) {
+ $this->db->select('*');
+ $this->db->from('student');
+ $this->db->where('user_key =',$student_id);
+ $query = $this->db->get();
+ return $data = $query->result();
+}
 
-    private function checkIfIsExistInStudentPoint($student_id,$material_id){
-        $this->db->select('*');
-        $this->db->from('student_point');
-        $this->db->join('student', 'student.id = student_point.student_id');
-        $this->db->where('student.user_key =', $student_id);
-        $this->db->where('material_id =', $material_id);
-        $query = $this->db->get();
+public function set_student_point_model($student_id, $material_num, $point,$reason){
+
+ $this->db->select('id,point');
+ $this->db->from('materials');
+ $this->db->where('material_number =', $material_num);
+ $this->db->limit(1);
+ $query = $this->db->get();
+ $data = $query->result();
+
+ if (empty($reason)) {
+  $data = array('student_id' => $student_id,
+    'material_id' => $data[0]->id,
+    'point' => $data[0]->point,
+    'reason' => ''
+  );
+  $query =  $this->db->insert('student_point', $data);
+  return ($this->db->affected_rows() != 1) ? false : true;
+
+}else {
+  $data = array('student_id' => $student_id,
+    'material_id' => $data[0]->id,
+    'point' => $point,
+    'reason' => $reason
+  );
+  $query =  $this->db->insert('student_point', $data);
+  return ($this->db->affected_rows() != 1) ? false : true;
+}
+
+}
+
+private function checkIfIsExistInStudentPoint($student_id,$material_id){
+  $this->db->select('*');
+  $this->db->from('student_point');
+  $this->db->join('student', 'student.id = student_point.student_id');
+  $this->db->where('student.user_key =', $student_id);
+  $this->db->where('material_id =', $material_id);
+  $query = $this->db->get();
 
         // $data = $query->result();
-        if ($query->num_rows() != 1) {
+  if ($query->num_rows() != 1) {
                     // there should only be one row - anything else is an error
-            return false;
-        }else {
-            return true;
-        }
-        
-    }
+    return false;
+  }else {
+    return true;
+  }
+
+}
 
 
 }
